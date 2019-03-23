627d563229153126e56353efeea52b697a33c879
rahul <rahulpatil97388@gmail.com>
2019-03-23 19:54:21 +0530

    commit is necessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SigninComponent } from './signin/signin.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LanguagesComponent } from './languages/languages.component';
+
+const Router:Routes =[
+  {path:'features', component:FeaturesComponent},
+  {path:'pricing', component:PricingComponent},
+  {path:'signin', component:SigninComponent},
+  {path:'languages', component:LanguagesComponent},
+  {path:'**', component:PagenotfoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    PricingComponent,
+    SigninComponent,
+    PagenotfoundComponent,
+    LanguagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(Router)
   ],
   providers: [],
   bootstrap: [AppComponent]
